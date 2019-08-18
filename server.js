8cf9992b66d1eb9b04e75797dce652a9ad372262
dahamv <dahamviharana@gmail.com>
2019-08-18 10:59:50 +0000

    changed server.js to suit Heroku

--- server.js
+++ server.js
@@ -13,4 +13,7 @@ res.sendFile(path.join(__dirname+'/dist/real-estate-fe/index.html'));
 });
 
 // Start the app by listening on the default Heroku port
-app.listen(process.env.PORT || 8080);
+//app.listen(process.env.PORT || 8080);
+app.listen(process.env.PORT || 3000, function(){
+  console.log("Express server listening on port %d in %s mode", this.address().port, app.settings.env);
+});
