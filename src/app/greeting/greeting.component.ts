9259eb49e40202b7bf7061e96a11c2ffa952fbd4
dahamv <dahamviharana@gmail.com>
2019-08-30 04:07:05 +0000

    moved the greeting stuff from home component to greeting component

--- src/app/greeting/greeting.component.ts
+++ src/app/greeting/greeting.component.ts
@@ -8,12 +8,20 @@ import { HelloworldService } from '../helloworld/helloworld.service';
 })
 export class GreetingComponent implements OnInit {
 
-   message: MessageModel;
+  message: MessageModel;
   name: String = "";
+
+  contacts;
+  selectedContact;
+  contact : {id, name, description, email} = {id: null, name: "", description: "", email: ""};
+  dataService : DataService;
 
   constructor(private data: HelloworldService) { }
 
-  ngOnInit() {}
+  ngOnInit() {
+      this.dataService = new DataService();
+      this.contacts = this.dataService.getContacts();
+  }
 
   onInput(event: any) {
     this.name =  event.target.value;
@@ -21,6 +29,17 @@ export class GreetingComponent implements OnInit {
 
   onClick(){
     this.data.getJSONResponse(this.name).subscribe(data => {this.message = data;});
+  }
+
+  public selectContact(contact){
+    this.selectedContact = contact;
+  }
+
+  createContact(){
+    console.log(this.contact);
+    this.dataService.createContact(this.contact);
+    this.contact = {id: null, name: "", description: "", email: ""};
+
   }
 }
 
@@ -31,4 +50,23 @@ export class MessageModel {
         this.id = _id;
         this.content = message;
     }
+}
+
+export class DataService {
+
+  contacts = [
+    {id: 1, name: "Contact 001", description: "Contact 001 des", email: "c001@email.com"},
+    {id: 2, name: "Contact 002", description: "Contact 002 des", email: "c002@email.com"},
+    {id: 3, name: "Contact 003", description: "Contact 003 des", email: "c003@email.com"},
+    {id: 4, name: "Contact 004", description: "Contact 004 des", email: "c004@email.com"}
+  ];
+
+  constructor() { }
+
+  public getContacts():Array<{id, name, description, email}>{
+    return this.contacts;
+  }
+  public createContact(contact: {id, name, description, email}){
+    this.contacts.push(contact);
+  }
 }
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -7,44 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
-  contacts;
-  selectedContact;
-  contact : {id, name, description, email} = {id: null, name: "", description: "", email: ""};
-  dataService : DataService;
   constructor() {}
 
-  ngOnInit() {
-      this.dataService = new DataService();
-      this.contacts = this.dataService.getContacts();
-  }
-
-  public selectContact(contact){
-    this.selectedContact = contact;
-  }
-
-  createContact(){
-    console.log(this.contact);
-    this.dataService.createContact(this.contact);
-    this.contact = {id: null, name: "", description: "", email: ""};
-
-  }
-}
-
-export class DataService {
-
-  contacts = [
-    {id: 1, name: "Contact 001", description: "Contact 001 des", email: "c001@email.com"},
-    {id: 2, name: "Contact 002", description: "Contact 002 des", email: "c002@email.com"},
-    {id: 3, name: "Contact 003", description: "Contact 003 des", email: "c003@email.com"},
-    {id: 4, name: "Contact 004", description: "Contact 004 des", email: "c004@email.com"}
-  ];
-
-  constructor() { }
-
-  public getContacts():Array<{id, name, description, email}>{
-    return this.contacts;
-  }
-  public createContact(contact: {id, name, description, email}){
-    this.contacts.push(contact);
-  }
+  ngOnInit() {}
 }
